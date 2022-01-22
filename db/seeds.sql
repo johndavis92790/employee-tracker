be3594497b08d4fc0b8032920bbd60789a27b7cb
johndavis <johndavis92790@gmail.com>
2022-01-22 15:31:09 -0700

    first commit, created basic db files

---
+++ db/seeds.sql
@@ -0,0 +1,14 @@
+INSERT INTO department
+  (name)
+VALUES
+  ('Sales');
+
+INSERT INTO role
+  (title, salary, department_i)
+VALUES
+  ('Sales Manager', 100000, 1);
+
+INSERT INTO employee
+  (first_name, last_name, role_id, manager_id)
+VALUES
+  ('John', 'Doe', 1, 1);
