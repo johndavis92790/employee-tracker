dbbcc87fbd51ee7d048d53d384b640ab83eb09bc
johndavis <johndavis92790@gmail.com>
2022-01-23 14:30:17 -0700

    fixed async functions

--- index.js
+++ index.js
@@ -1,43 +1,22 @@
-// GIVEN a command-line application that accepts user input
-// WHEN I start the application
-// THEN I am presented with the following options: view all departments, view all roles, view all employees, 
-      // add a department, add a role, add an employee, and update an employee role
-// WHEN I choose to view all departments
-// THEN I am presented with a formatted table showing department names and department ids
-// WHEN I choose to view all roles
-// THEN I am presented with the job title, role id, the department that role belongs to, and the salary for that role
-// WHEN I choose to view all employees
-// THEN I am presented with a formatted table showing employee data, including employee ids, first names, last names, job titles, 
-      // departments, salaries, and managers that the employees report to
-// WHEN I choose to add a department
-// THEN I am prompted to enter the name of the department and that department is added to the database
-// WHEN I choose to add a role
-// THEN I am prompted to enter the name, salary, and department for the role and that role is added to the database
-// WHEN I choose to add an employee
-// THEN I am prompted to enter the employee’s first name, last name, role, and manager, and that employee is added to the database
-// WHEN I choose to update an employee role
-// THEN I am prompted to select an employee to update and their new role and this information is updated in the database
-
 const inquirer = require('inquirer');
 const consoleTable = require('console.table');
 const mysql = require('mysql2');
 const db = require('./db/connection');
-
-//required classes and functions from other files
-// const Engineer = require('./lib/Engineer.js');
-// const Intern = require('./lib/Intern.js');
-// const Manager = require('./lib/Manager.js');
-// const { writeHtml } = require('./src/generatePage');
-
-//global variable
-// var employees = [];
 
 const initialPrompt = () => {
   return inquirer.prompt([ 
     {
       type: 'checkbox',
       name: 'type',
-      choices: ['View all departments', 'View all roles', 'View all employees', 'Add a department', 'Add a role', 'Add an employee', 'Update an employee role']
+      choices: ['View all departments', 'View all roles', 'View all employees', 'Add a department', 'Add a role', 'Add an employee', 'Update an employee role'],
+      validate: choiceInput => {
+        if (choiceInput) {
+          return true;
+        } else {
+          console.log(`Please make a choice!`);
+          return false;
+        }
+      }
     }
   ])
   .then(choice => {
@@ -52,9 +31,9 @@ const initialPrompt = () => {
     } else if (choice.type[0] === 'Add a role'){
       addRole();
     } else if (choice.type[0] === 'Add an employee'){
-      getRoles();
+      addEmp()
     } else if (choice.type[0] === 'Update an employee role'){
-      getEmpNames();
+      updateEmp();
     }
   });
 };
@@ -62,7 +41,6 @@ const initialPrompt = () => {
 const viewAllDept = () => {
   db.promise().query('SELECT * FROM department;')
   .then( ([rows]) => {
-    console.log(rows);
     console.table(rows);
   })
   .catch(console.log)
@@ -141,8 +119,8 @@ const addDept = () => {
   });
 };
 
-// salary, and department
-const addRole = () => {
+async function addRole() {
+  var deptArray  = await getDepts;
   return inquirer.prompt([ 
     {
       type: 'input',
@@ -171,25 +149,29 @@ const addRole = () => {
       }
     },
     {
-      type: 'input',
+      type: 'checkbox',
       name: 'roleDept',
       message: `What the role's department? (Required)`,
       validate: roleDeptInput => {
         if (roleDeptInput) {
           return true;
         } else {
-          console.log(`Please enter the role's department!`);
+          console.log(`Please choose the role's department!`);
           return false;
         }
-      }
-    },
+      },
+      choices: deptArray
+    }
   ])
   .then(role => {
+    var dept = role.roleDept;
+    var newDept = dept.toString(dept);
+    var deptArray = newDept.split(' ');
     db.promise().query(`
     INSERT INTO role
       (title, salary, department_id)
     VALUES
-      ('${role.roleTitle}', ${role.roleSalary}, ${role.roleDept});`
+      ('${role.roleTitle}', ${role.roleSalary}, ${deptArray[0]});`
     )
     .catch(console.log)
     .then( () => {
@@ -199,8 +181,9 @@ const addRole = () => {
   });
 };
 
-const addEmp = (rolesArray, managersArray) => {
-  console.log('#6', rolesArray);
+async function addEmp() {
+  var rolesArray = await getRoles;
+  var managersArray = await getManagerNames;
   return inquirer.prompt([ 
     {
       type: 'input',
@@ -256,7 +239,7 @@ const addEmp = (rolesArray, managersArray) => {
         if (empManagerInput) {
           return true;
         } else {
-          console.log(`Please enter the employee's manager!`);
+          console.log(`Please choose the employee's manager!`);
           return false;
         }
       },
@@ -265,7 +248,6 @@ const addEmp = (rolesArray, managersArray) => {
     }
   ])
   .then(emp => {
-    console.log(emp);
     var role = emp.empRole;
     var newRole = role.toString(role);
     var roleArray = newRole.split(' ');
@@ -300,7 +282,9 @@ const addEmp = (rolesArray, managersArray) => {
   });
 };
 
-const updateEmp = (empArray) => {
+async function updateEmp() {
+  var empArray = await getEmpNames;
+  var rolesArray = await getRoles;
   return inquirer.prompt([
     {
         type: 'checkbox',
@@ -308,27 +292,30 @@ const updateEmp = (empArray) => {
         choices: empArray
     },
     {
-      type: 'input',
+      type: 'checkbox',
       name: 'empNewRole',
       message: `What is the employee's new role? (Required)`,
       validate: empNewRoleInput => {
         if (empNewRoleInput) {
           return true;
         } else {
-          console.log(`Please enter the employee's new role!`);
+          console.log(`Please choose the employee's new role!`);
           return false;
         }
-      }
+      },
+      choices: rolesArray
     }
   ])
   .then(emp => {
     var name = emp.type;
     var newName = name.toString(name);
     var nameArray = newName.split(' ');
+    var role = emp.empNewRole;
+    var newRole = role.toString(role);
+    var roleArray = newRole.split(' ');
     db.promise().query(`
     UPDATE employee
-    SET role_id = ${emp.empNewRole}
-    WHERE id = ${nameArray[0]};`
+    SET role_id = ${roleArray[0]} WHERE id = ${nameArray[0]};`
     )
     .catch(console.log)
     .then( () => {
@@ -338,27 +325,39 @@ const updateEmp = (empArray) => {
   });
 };
 
-function getRoles(){
+const getDepts = new Promise(function(resolve){
+  db.promise().query(`SELECT * FROM department;`)
+  .then( ([rows]) => {
+    var deptArray = [];
+    rows.forEach((dept) => {
+      deptArray.push(dept.id + ' ' + dept.name);
+    });
+    return deptArray;
+  })
+  .catch(console.log)
+  .then( (deptArray) => {
+    resolve(deptArray);
+  });
+});
+
+const getRoles = new Promise(function(resolve){
   db.promise().query(`SELECT * FROM role;`)
   .then( ([rows]) => {
     var rolesArray = [];
     rows.forEach((roles) => {
       rolesArray.push(roles.id + ' ' + roles.title);
     });
-    console.log('#1', rolesArray);
     return rolesArray;
   })
   .catch(console.log)
   .then( (rolesArray) => {
-    console.log('#2', rolesArray);
-    getManagerNames(rolesArray);
+    resolve(rolesArray);
   });
-}
+});
 
-function getEmpNames(){
+const getEmpNames = new Promise(function(resolve){
   db.promise().query(`SELECT * FROM employee;`)
   .then( ([rows]) => {
-    // console.log(rows);
     var namesArray = [];
     rows.forEach((name) => {
       namesArray.push(name.id + ' ' + name.first_name + ' ' + name.last_name);
@@ -367,12 +366,11 @@ function getEmpNames(){
   })
   .catch(console.log)
   .then( (list) => {
-    updateEmp(list);
+    resolve(list);
   });
-}
+});
 
-function getManagerNames(rolesArray){
-  console.log('#3', rolesArray);
+const getManagerNames = new Promise(function(resolve){
   db.promise().query(`
   SELECT *
   FROM employee e 
@@ -383,200 +381,12 @@ function getManagerNames(rolesArray){
     rows.forEach((name) => {
       managersArray.push(name.id + ' ' + name.first_name + ' ' + name.last_name);
     });
-    console.log('#4', rolesArray);
-    return rolesArray, managersArray;
+    return managersArray;
   })
   .catch(console.log)
-  .then( (rolesArray, managersArray) => {
-    console.log('#5', rolesArray);
-    addEmp(rolesArray, managersArray);
+  .then( (managersArray) => {
+    resolve(managersArray);
   });
-}
+});
 
 initialPrompt();
-
-// initial prompts, globally used for all employees
-// const promptUser = (type) => {
-//   return inquirer.prompt([
-//     {
-//       type: 'input',
-//       name: 'employeeName',
-//       message: `What the ${type}'s name? (Required)`,
-//       validate: nameInput => {
-//         if (nameInput) {
-//           return true;
-//         } else {
-//           console.log(`Please enter the ${type}'s name!`);
-//           return false;
-//         }
-//       }
-//     },
-//     {
-//       type: 'input',
-//       name: 'employeeID',
-//       message: `What the ${type}'s ID #? (Required)`,
-//       validate: idInput => {
-//         if (idInput) {
-//           return true;
-//         } else {
-//           console.log(`Please enter the ${type}'s ID #!`);
-//           return false;
-//         }
-//       }
-//     },
-//     {
-//       type: 'input',
-//       name: 'employeeEmail',
-//       message: `What the ${type}'s email? (Required)`,
-//       validate: emailInput => {
-//         if (emailInput) {
-//           return true;
-//         } else {
-//           console.log(`Please enter the ${type}'s email!`);
-//           return false;
-//         }
-//       }
-//     }
-//   ]);
-// };
-
-// //manager-only prompts
-// const promptManager = employeeData =>{
-//   employeeData.type = 'Manager'
-//   if (!employees.managers) {
-//     employees.managers = [];
-//   }
-//   return inquirer.prompt([ 
-//     {
-//       type: 'input',
-//       name: 'officeNumber',
-//       message: `What the manager's office #? (Required)`,
-//       validate: officeInput => {
-//         if (officeInput) {
-//           return true;
-//         } else {
-//           console.log(`Please enter the manager's office #!`);
-//           return false;
-//         }
-//       }
-//     }
-//   ])
-//   .then(managerData => {
-//     employeeData.officeNumber = managerData.officeNumber;
-//     const manager = new Manager(
-//       employeeData.employeeName,
-//       employeeData.employeeID,
-//       employeeData.employeeEmail,
-//       employeeData.officeNumber
-//     );
-//     employees.managers.push(manager);
-//     return employeeData;
-//   })
-//   .then(promptAnother);
-// }
-
-// //engineer-only prompts
-// const promptEngineer = employeeData => {
-//   if (!employees.engineers) {
-//     employees.engineers = [];
-//   }
-//   return inquirer.prompt([ 
-//     {
-//       type: 'input',
-//       name: 'github',
-//       message: `What the eningeer's github? (Required)`,
-//       validate: githubInput => {
-//         if (githubInput) {
-//           return true;
-//         } else {
-//           console.log(`Please enter the eningeer's github!`);
-//           return false;
-//         }
-//       }
-//     }
-//   ])
-//   .then(engineerData => {
-//     employeeData.github = engineerData.github;
-//     const engineer = new Engineer(
-//       employeeData.employeeName,
-//       employeeData.employeeID,
-//       employeeData.employeeEmail,
-//       employeeData.github
-//     );
-//     employees.engineers.push(engineer);
-//     return employeeData;
-//   })
-//   .then(promptAnother);
-// }
-
-// //intern-only prompts
-// const promptIntern = employeeData => {
-//   if (!employees.interns) {
-//     employees.interns = [];
-//   }
-//   return inquirer.prompt([ 
-//     {
-//       type: 'input',
-//       name: 'school',
-//       message: `What the intern's school name? (Required)`,
-//       validate: schoolInput => {
-//         if (schoolInput) {
-//           return true;
-//         } else {
-//           console.log(`Please enter the intern's school name!`);
-//           return false;
-//         }
-//       }
-//     }
-//   ])
-//   .then(internData => {
-//     employeeData.school = internData.school;
-//     const intern = new Intern(
-//       employeeData.employeeName,
-//       employeeData.employeeID,
-//       employeeData.employeeEmail,
-//       employeeData.school
-//     );
-//     employees.interns.push(intern);
-//     return employeeData;
-//   })
-//   .then(promptAnother);
-// }
-
-// //prompts to determine if another employee is needed, 
-// //if yes, which kind?
-// //if no, finishes app and creates object to send to create index.html file
-// const promptAnother = () => {
-//   return inquirer.prompt([ 
-//     {
-//       type: 'confirm',
-//       name: 'confirmAnother',
-//       message: 'Would you like to add another employee?',
-//       default: false
-//     },
-//     {
-//       type: 'checkbox',
-//       name: 'type',
-//       message: 'Is the new employee going to be an engineer or intern? (Check all that apply)',
-//       choices: ['Engineer', 'Intern'],
-//       when: ({ confirmAnother }) => confirmAnother
-//     }
-//   ])
-//   .then(choice => {
-//     if (choice.confirmAnother){
-//       if (choice.type[0] === 'Engineer') {
-//         promptUser('engineer')
-//           .then(promptEngineer)
-//       } else {
-//         promptUser('intern')
-//           .then(promptIntern)
-//       }
-//     } else {
-//       console.log('employees', employees);
-//       writeHtml(employees)
-//     }
-//   });
-// }
-
-// //initial function
-// promptUser('manager').then(promptManager);
